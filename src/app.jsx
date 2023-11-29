686f26c3550dbddd1615be40f638dc1ee2c59032
DaviiJones <davonbridgett@pursuit.org>
2023-11-28 21:47:36 -0500

    NAVBAR

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import NavBar from "./Components/NavBar.jsx";
+import NavBar from "./Components/NavBar";
 import FourOFour from "./Pages/FourOFour";
 import Home from "./Pages/Home";
 import Missions from './Pages/Missions';
